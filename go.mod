c1492b3cd22f134e188f9dbbf619b553ae948a7e
bhushan-aruto <nagabhushanbhandary524@gmail.com>
2025-07-07 22:11:14 +0530

    the task entity structure is done

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/bhushan-aruto/go-task-manager
+
+go 1.23.5
+
+require go.mongodb.org/mongo-driver v1.17.4
---
+++ internal/entity/task.go
@@ -0,0 +1,29 @@
+package entity
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Task struct {
+	ID          primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	UserId      string             `json:"user_id" bson:"user_id"`
+	Title       string             `json:"title" bson:"title"`
+	Description string             `json:"description" bson:"description"`
+	Completed   bool               `json:"completed_at" bson:"completed_at"`
+	CreatedAt   time.Time          `json:"created_at" bson:"created_at"`
+	UpdatedAt   time.Time          `json:"updated_at" bson:"updated_at"`
+}
+
+func NewTask(userId, title, description string) *Task {
+	return &Task{
+		ID:          primitive.NewObjectID(),
+		UserId:      userId,
+		Title:       title,
+		Description: description,
+		Completed:   false,
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+}
